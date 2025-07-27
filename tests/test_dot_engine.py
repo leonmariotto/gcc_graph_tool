c5e9c5ff9c4352b39a8f194d49df474ab27e74f4
Leon Mariotto <lmariott@student.42.fr>
2025-07-27 21:14:45 +0200

    fix dummy test

--- tests/test_dot_engine.py
+++ tests/test_dot_engine.py
@@ -2,5 +2,6 @@ from scripts.DotEngine import DotEngine
 
 
 def test_dot_engine():
-    dot_engine = DotEngine("resources/dots/list_test.c.callgraph.dot")
+    dot_engine = DotEngine()
+    dot_engine.parse_dot("resources/dots/list_test.c.callgraph.dot")
     dot_engine.modify_nodes_label()
